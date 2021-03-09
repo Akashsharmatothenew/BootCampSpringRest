f221a2fb130000fd1ca90df4aec782095e95ee8e
Akashsharmatothenew <akash.sharma1@tothenew.com>
2021-03-09 15:55:49 +0530

    Q1complete

---
+++ RestSpring/src/main/java/com/SpringwithRest1/RestSpring/helloworld/Q1HelloWorld.java
@@ -0,0 +1,45 @@
+package com.SpringwithRest1.RestSpring.helloworld;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.MessageSource;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.Locale;
+
+/**
+ * Created by ttn on 7/3/21.
+ */
+//Controler
+    @RestController
+public class Q1HelloWorld {
+
+
+        //internatization
+    @Autowired
+    MessageSource messageSource;
+        //Get
+         //URI -/Q1
+
+    //method
+    //@RequestMapping(method = RequestMethod.GET, path = "/Q1")
+
+    @GetMapping(path="/Q1hello")
+    public String displayHello(){
+        return "Hello world";
+    }
+
+    //create object or return bean
+    @GetMapping(path = "/Q1")
+    public HelloWorldBean helloworldbean(){
+        return new HelloWorldBean("Hello World");
+    }
+    //pathvariable
+    @GetMapping(path ="/Q1/{name}")
+    public HelloWorldBean helloWorldPathvariable(@PathVariable String name){
+        return new HelloWorldBean(String.format("Hello World, %s ",name));
+    }
+    @GetMapping(path ="/hello-world-internationalized")
+    public String helloWorldInternalization(@RequestHeader(name="Accept-Language",required = false) Locale locale){
+        return messageSource.getMessage("good.morning.message",null,locale);
+    }
+}
